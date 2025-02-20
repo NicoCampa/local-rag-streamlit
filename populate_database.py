40eceacf0634bca97158462325b0509e6aaa6f93
NicoCampa <nicolocampagnoli20@icloud.com>
2025-02-20 23:00:34 +0100

    c4

--- populate_database.py
+++ populate_database.py
@@ -57,8 +57,8 @@ def load_documents():
 
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=80,
+        chunk_size=1000,
+        chunk_overlap=200,
         length_function=len,
         is_separator_regex=False,
     )
